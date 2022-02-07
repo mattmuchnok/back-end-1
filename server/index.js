fdb874a346859fed65627122b3cc97989c959a0f
mattmuchnok <mattmuchnok@gmail.com>
2022-02-07 16:19:09 -0500

    final commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require(`express`)
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.get(`/api/users`, (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+})
+
+app.get(`/weather/:temperature`, (req, res) => {
+    const { temperature } = req.params
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`
+    res.status(200).send(phrase)
+})
+
+const SERVER_PORT = 4000
+
+app.listen(SERVER_PORT, () => console.log(`Server running on port 4000`))
